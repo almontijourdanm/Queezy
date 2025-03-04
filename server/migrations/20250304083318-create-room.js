ecfb6405e2245f44ff15b104781b3758ab9f5525
dickydestyan <dickydestyan03@gmail.com>
2025-03-04 16:28:18 +0700

    setup migration and models done

---
+++ server/migrations/20250304083318-create-room.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Rooms', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      HostId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id"
+        }
+      },
+      status: {
+        type: Sequelize.STRING,
+        defaultValue: "Waiting"
+      },
+      questions: {
+        type: Sequelize.JSONB
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Rooms');
+  }
+};
