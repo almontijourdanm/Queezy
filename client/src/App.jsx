2617534380ae42f47035ab91443fa9f26f22c078
newMuhilka <m.i.kamil619@gmail.com>
2025-03-04 17:29:08 +0700

    refactor(client): simplify the instructions page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,9 @@
-import { useState } from 'react'
 import Instructions from './pages/Instructions'
 
 export default function App() {
-  const [showInstructions, setShowInstructions] = useState(true);
-
   return (
-    <>
-      <div className="p-4">
-        {showInstructions && <Instructions onClose={() => setShowInstructions(false)} />}
-        <button className="btn btn-secondary" onClick={() => setShowInstructions(true)}>
-          Show Instructions
-        </button>
-      </div>
-    </>
+    <div className="p-4">
+      <Instructions />
+    </div>
   )
 }
